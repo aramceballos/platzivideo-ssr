fe5aeca4c52e250c295cc6fb1bb794472df393dc
Aram Ceballos <aramgonzalez12@hotmail.com>
2020-05-15 16:48:34 -0700

    WDS & HMR implemented

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 require('@babel/register')({
   presets: ['@babel/preset-env', '@babel/preset-react'],
 });
+require('dotenv').config();
 
 require('./server');
--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,29 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
 const express = require('express');
+const webpack = require('webpack');
 
 const app = express();
+
+const { PORT, ENV } = process.env;
+
+if (ENV === 'development') {
+  console.log('Development config');
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig);
+  const serverConfig = { port: PORT, hot: true };
+
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webpackHotMiddleware(compiler));
+}
 
 app.get('*', (req, res) => {
   res.send({ hello: 'world' });
 });
 
-app.listen(4000, (err) => {
+app.listen(PORT, (err) => {
   if (err) console.error(err);
-  else console.log('Server running at http://localhost:4000');
+  else console.log(`Server running at http://localhost:${PORT}`);
 });
