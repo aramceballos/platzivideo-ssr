1342b392e6d0c0f8387b64cc20cba4e645e9f999
Aram Ceballos <aramgonzalez12@hotmail.com>
2020-05-18 15:36:52 -0700

    Fixing eslint issues

--- src/frontend/containers/Player.jsx
+++ src/frontend/containers/Player.jsx
@@ -4,34 +4,37 @@ import { getVideoSource } from '../actions';
 import '../assets/styles/components/Player.scss';
 import NotFound from './NotFound';
 
-const Player = props => {
-  const { id } = props.match.params;
-  const hasPlaying = Object.keys(props.playing).length > 0;
+const Player = (props) => {
+  const { match } = props;
+  const { id } = match.params;
+  const hasPlaying = Object.keys(playing).length > 0;
   useEffect(() => {
-    props.getVideoSource(id);
+    getVideoSource(id);
   }, []);
   return hasPlaying ? (
-    <div className="Player">
+    <div className='Player'>
       <video controls autoPlay>
-        <source src={props.playing.source} type="video/mp4" />
+        <source src={playing.source} type='video/mp4' />
       </video>
-      <div className="Player-back">
-        <button type="button" onClick={() => props.history.goBack()}>
+      <div className='Player-back'>
+        <button type='button' onClick={() => props.history.goBack()}>
           Regresar
         </button>
       </div>
     </div>
-  ) : <NotFound />;
+  ) : (
+    <NotFound />
+  );
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     playing: state.playing,
-  }
-}
+  };
+};
 
 const mapDispatchToProps = {
-  getVideoSource, 
-}
+  getVideoSource,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Player);
