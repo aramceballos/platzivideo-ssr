b187b5e71faaf7ae017a523bab73ac5217159d6b
Aram Ceballos <aramgonzalez12@hotmail.com>
2020-05-17 17:08:30 -0700

    Applying server side render

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable global-require */
-const express = require('express');
-const webpack = require('webpack');
+import express from 'express';
+import webpack from 'webpack';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { StaticRouter } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
+import reducer from '../frontend/reducers';
+import initialState from '../frontend/initialState';
+import serverRoutes from '../frontend/routes/serverRoutes';
 
 const app = express();
 
@@ -19,8 +28,8 @@ if (ENV === 'development') {
   app.use(webpackHotMiddleware(compiler));
 }
 
-app.get('*', (req, res) => {
-  res.send(`
+const setResponse = (html) => {
+  return `
   <!DOCTYPE html>
   <html lang="en">
     <head>
@@ -30,13 +39,27 @@ app.get('*', (req, res) => {
       <title>Platzi Video</title>
     </head>
     <body>
-      <div id="app"></div>
+      <div id="app">${html}</div>
       <script src="assets/app.js" type="text/javascript"></script>
     </body>
   </html>
-  
-  `);
-});
+  `;
+};
+
+const renderApp = (req, res) => {
+  const store = createStore(reducer, initialState);
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        {renderRoutes(serverRoutes)}
+      </StaticRouter>
+    </Provider>,
+  );
+
+  res.send(setResponse(html));
+};
+
+app.get('*', renderApp);
 
 app.listen(PORT, (err) => {
   if (err) console.error(err);
