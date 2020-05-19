8f5e086cdfd804ced59baf2b1a78ba40498c467d
Aram Ceballos <aramgonzalez12@hotmail.com>
2020-05-18 18:35:25 -0700

    Install and configuration of redux thunk

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { createBrowserHistory } from 'history';
 import { Router } from 'react-router';
 import reducer from './reducers';
@@ -11,7 +12,11 @@ const preloadedState = window.__PRELOADED_STATE__;
 
 const history = createBrowserHistory();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, preloadedState, composeEnhancers());
+const store = createStore(
+  reducer,
+  preloadedState,
+  composeEnhancers(applyMiddleware(thunk)),
+);
 
 delete window.__PRELOADED_STATE__;
 
